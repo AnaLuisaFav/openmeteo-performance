df7075db4fbd193a70a6bc814c4a38fd92ef6724
Ana Luisa <analuisafav@gmail.com>
2025-10-15 21:03:08 -0300

    tests: added stress test

--- scripts/smoke-test.js
+++ scripts/smoke-test.js
@@ -1,19 +1,24 @@
 import http from "k6/http";
-import { check, sleep } from "k6";
+import { sleep } from "k6";
 import { cities } from "../helpers/cities.js";
-import { BASE_URL, THRESHOLDS } from "../helpers/config.js";
+import { validateWeatherResponse } from "../helpers/checks.js"
+import { BASE_URL, DEFAULT_PARAMS } from "../helpers/config.js";
 
 export const options = {
   vus: 1,
   duration: "5s",
-  thresholds: THRESHOLDS
+  thresholds: {
+    http_req_duration: ["p(95)<800"],
+    http_req_failed: ["rate<0.05"],
+  },
 };
 
 export default function () {
   const city = cities[Math.floor(Math.random() * cities.length)];
-  const url = `${BASE_URL}?latitude=${city.lat}&longitude=${city.lon}&hourly=temperature_2m,precipitation,cloudcover&timezone=America%2FRecife`;
+  const url = `${BASE_URL}?latitude=${city.lat}&longitude=${city.lon}${DEFAULT_PARAMS}`;
 
   const res = http.get(url);
-  check(res, { "status 200": (r) => r.status === 200 });
+  validateWeatherResponse(res);
+
   sleep(1);
 }
